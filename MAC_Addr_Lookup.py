23a4cf59b8f616b095dfc70d504b264ba084de66
Victoria Rivas <vrivas@knights.ucf.edu>
2019-08-17 19:43:47 -0400

    Updated MAC address input and Dockerfile

--- MAC_Addr_Lookup.py
+++ MAC_Addr_Lookup.py
@@ -1,13 +1,15 @@
+#! Python 3
 # Prints the company associated with the given MAC address
 
 import requests, json, sys
 
-# If the user does not enter a MAC address in the command line, exit the script
+# If the user does not enter a MAC address in the command line,
+# request MAC address
 if len(sys.argv) < 2:
-    print("Usage: MAC_Addr_Lookup.py MAC_Address")
-    sys.exit()
-
-addr = sys.argv[1]
+    addr = input("Please enter a MAC address: ")
+# Otherwise, the address may be in the command line
+else:
+    addr = sys.argv[1]
 
 # Formatted string of URL for request with MAC address
 url = 'https://api.macaddress.io/v1?apiKey=at_dgUtmV9XwuiO9hs4JqsU6tzctGMbN&output=json&search=%s' % (addr)
